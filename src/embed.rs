247b5f0cbdc7f53790c512548049a40874d6528e
gongzhengyang <gongzhengyang@bolean.com.cn>
2023-11-14 10:57:52 +0800

    feat: add precompressed for server dir

--- src/embed.rs
+++ src/embed.rs
@@ -1,4 +1,8 @@
+use std::path::Path;
+
+use async_compression::{tokio::write::BrotliEncoder, Level};
 use rust_embed::RustEmbed;
+use tokio::io::AsyncWriteExt;
 use tower_http::services::ServeDir;
 
 /// used with swagger openapi
@@ -7,14 +11,17 @@ use tower_http::services::ServeDir;
 /// use awesome_operates::embed::server_dir;
 /// use awesome_operates::swagger::InitSwagger;
 /// use axum::Router;
+/// use tower::ServiceBuilder;
+/// use tower_http::compression::CompressionLayer;
 ///
 /// #[tokio::test]
 /// async fn server() -> anyhow::Result<()> {
 ///     awesome_operates::extract_all_files!(awesome_operates::embed::Asset);
 ///     let extract_dir_path = "embed_files/swagger";
 ///     let app = Router::new()
-///         .nest_service("/docs/", server_dir(extract_dir_path))
-///         .nest_service("/swagger-api/", server_dir("swagger-files"));
+///         .nest_service("/docs/", server_dir(extract_dir_path).await.unwrap())
+///         .nest_service("/swagger-api/", server_dir("swagger-files").await.unwrap())
+///         .layer(ServiceBuilder::new().layer(CompressionLayer::new()));
 ///     InitSwagger::new(extract_dir_path, "swagger-init.js", "swagger.html", "./swagger-api/api.json").build().await.unwrap();
 ///     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 /// #    axum::serve(listener, app).await.unwrap();
@@ -27,10 +34,33 @@ use tower_http::services::ServeDir;
 #[folder = "src/embed_files/"]
 pub struct Asset;
 
-pub fn server_dir(dir_path: &str) -> ServeDir {
-    ServeDir::new(dir_path)
+pub async fn server_dir(dir_path: &str) -> anyhow::Result<ServeDir> {
+    pre_brotli_compress_dir(dir_path).await?;
+    Ok(ServeDir::new(dir_path)
         .precompressed_br()
         .precompressed_deflate()
         .precompressed_gzip()
-        .precompressed_zstd()
+        .precompressed_zstd())
+}
+
+pub async fn pre_brotli_compress_dir(dir: &str) -> anyhow::Result<()> {
+    for entry in walkdir::WalkDir::new(dir)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        if entry.path().is_file() {
+            brotli_compress(entry.path()).await?;
+        }
+    }
+    Ok(())
+}
+
+pub async fn brotli_compress(path: &Path) -> anyhow::Result<()> {
+    let data = tokio::fs::read(path).await?;
+    let mut encoder = BrotliEncoder::with_quality(Vec::new(), Level::Precise(4));
+    encoder.write_all(&data).await?;
+    encoder.shutdown().await?;
+    let compressed = encoder.into_inner();
+    tokio::fs::write(format!("{}.br", path.display()), compressed).await?;
+    Ok(())
 }
