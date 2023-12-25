9d31dd84e55738e972ed189f52798423b04a05bf
gongzhengyang <gongzhengyang@bolean.com.cn>
2023-12-25 17:18:27 +0800

    feat: tracing file split to multi fn for custom operate

--- src/log.rs
+++ src/log.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 
 use snafu::ResultExt;
-use tracing_appender::non_blocking::WorkerGuard;
+use tracing_appender::non_blocking::{NonBlocking, WorkerGuard};
 use tracing_appender::rolling::{RollingFileAppender, Rotation};
 use tracing_subscriber::{fmt, layer::SubscriberExt};
 
@@ -15,7 +15,7 @@ use crate::error::{CommonIoSnafu, LogFileBuildSnafu, Result, TracingSetGlobalSna
 ///  async fn main() {
 ///     // this is very important, must receive worker_guard value
 ///     // or file writer will take no reflect
-///     let _guard = tracing_both_file_stdout("target", "", "agent.log").await;
+///     let _guard = tracing_both_file_stdout("target", "", "agent.log", None).await;
 ///     // let _guard = tracing_both_file_stdout("logs", "", "agent.log").await;
 ///     tokio::time::sleep(Duration::from_secs(1)).await;
 ///     tokio::spawn(async {
@@ -23,34 +23,74 @@ use crate::error::{CommonIoSnafu, LogFileBuildSnafu, Result, TracingSetGlobalSna
 ///     });
 ///  }
 /// ```
-pub async fn tracing_both_file_stdout(
+
+pub async fn tracing_both_file_stdout
+(
     log_dir: impl AsRef<Path>,
     log_file_prefix: impl Into<String>,
     log_file_suffix: impl Into<String>,
-) -> Result<WorkerGuard> {
-    tokio::fs::create_dir_all(&log_dir)
-        .await
-        .context(CommonIoSnafu)?;
-    let file_appender = RollingFileAppender::builder()
-        .rotation(Rotation::DAILY)
-        .filename_prefix(log_file_prefix)
-        .filename_suffix(log_file_suffix)
-        .build(log_dir)
-        .context(LogFileBuildSnafu)?;
-    let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
-
+    rotation: Option<Rotation>,
+) -> Result<WorkerGuard>
+{
+    let (non_blocking, guard) = tracing_with_file(log_dir, log_file_prefix, log_file_suffix, rotation).await?;
     #[cfg(windows)]
         let ansi_enabled = false;
     #[cfg(unix)]
         let ansi_enabled = true;
 
     let collector = tracing_subscriber::registry()
-        .with(
-            fmt::Layer::new()
-                .with_ansi(ansi_enabled)
-                .with_writer(std::io::stdout),
-        )
+        .with(fmt::Layer::new().with_ansi(ansi_enabled).with_writer(std::io::stdout))
         .with(fmt::Layer::new().with_ansi(false).with_writer(non_blocking));
     tracing::subscriber::set_global_default(collector).context(TracingSetGlobalSnafu)?;
     Ok(guard)
 }
+
+pub async fn tracing_with_file(log_dir: impl AsRef<Path>,
+                               log_file_prefix: impl Into<String>,
+                               log_file_suffix: impl Into<String>,
+                               rotation: Option<Rotation>) -> Result<(NonBlocking, WorkerGuard)> {
+    tokio::fs::create_dir_all(&log_dir)
+        .await
+        .context(CommonIoSnafu)?;
+    let file_appender = RollingFileAppender::builder()
+        .rotation(rotation.unwrap_or(Rotation::DAILY))
+        .filename_prefix(log_file_prefix)
+        .filename_suffix(log_file_suffix)
+        .build(log_dir)
+        .context(LogFileBuildSnafu)?;
+    let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+    Ok((non_blocking, guard))
+}
+
+
+// pub trait CollectorFmtLayer<S>
+//     where
+//         S: tracing::Subscriber + for<'a> tracing_subscriber::registry::LookupSpan<'a>
+// {
+//     type L: Layer<S>;
+//     fn fmt_layer(&self) -> Self::L;
+// }
+//
+// pub struct StdoutLayer;
+//
+// impl<S: tracing::Subscriber + for<'a> tracing_subscriber::registry::LookupSpan<'a>> CollectorFmtLayer<S> for StdoutLayer {
+//     type L = fmt::Layer<S, DefaultFields, Format, fn() -> Stdout>;
+//
+//     fn fmt_layer(&self) -> Self::L {
+//         #[cfg(windows)]
+//             let ansi_enabled = false;
+//         #[cfg(unix)]
+//             let ansi_enabled = true;
+//         fmt::Layer::new().with_ansi(ansi_enabled).with_writer(std::io::stdout)
+//     }
+// }
+//
+// pub struct FileFmtLayer;
+//
+// impl<S: tracing::Subscriber + for<'a> tracing_subscriber::registry::LookupSpan<'a>> CollectorFmtLayer<S> for FileFmtLayer {
+//     type L = fmt::Layer<S>;
+//
+//     fn fmt_layer(&self) -> Self::L {
+//         fmt::Layer::new().with_ansi(false)
+//     }
+// }
